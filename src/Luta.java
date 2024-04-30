77467040aa27a2f367a9dce1604622e39fb86caa
W Cardoso <cardosu7@gmail.com>
2024-04-30 15:18:01 -0300

    criando uma luta

--- src/Luta.java
+++ src/Luta.java
@@ -1,16 +1,54 @@
 public class Luta {
 
-   private int desafiado;
-   private int desafiante;
+   private Lutador desafiado;
+   private Lutador desafiante;
    private int rounds;
-   private int aprovada;
+   private boolean aprovada;
 
 
    public void marcarLuta(){
+      if (11.this.ge)
+   }
+
+   public void lutar(){
+
+      if(aprovada){
+         desafiado.apresentar();
+         desafiante.apresentar();
+         vencedor = aleatório(0..2)
+      }
 
    }
 
-   public void luta(){
+   public Lutador getDesafiado() {
+      return desafiado;
+   }
 
+   public void setDesafiado(Lutador dd) {
+      this.desafiado = dd;
+   }
+
+   public Lutador getDesafiante() {
+      return desafiante;
+   }
+
+   public void setDesafiante(Lutador de) {
+      this.desafiante = de;
+   }
+
+   public int getRounds() {
+      return rounds;
+   }
+
+   public void setRounds(int rounds) {
+      this.rounds = rounds;
+   }
+
+   public boolean isAprovada() {
+      return aprovada;
+   }
+
+   public void setAprovada(boolean aprovada) {
+      this.aprovada = aprovada;
    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
 
         l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
 
-        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 69.8f, 14, 2, 3);
+
+        l[2] = new Lutador("Anderson", "Paraguay", 19, 1.78f, 87.8f, 14, 2, 3);
+
+        l[3] = new Lutador("Junin", "Alemanha", 39, 1.50f, 85.8f, 14, 2, 3);
+
+        l[4] = new Lutador("CaoLouco", "EUA", 25, 1.67f, 77.8f, 14, 2, 3);
+
+        l[5] = new Lutador("MalucoDoDegrau", "Japão", 32, 1.88f, 67.8f, 14, 2, 3);
 
         l[1].apresentar();
 
